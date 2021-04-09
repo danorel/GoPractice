c70459178de51329dd9afaff6e72bf84d908095d
danorel <mail.ordan@gmail.com>
2021-04-09 14:45:49 +0300

    [Slice & Arrays]: Finished learning the basics of the slice & array behavior.

--- src/github.com/danorel/GoApp/Main.go
+++ src/github.com/danorel/GoApp/Main.go
@@ -9,10 +9,39 @@ const (
 	hasAccessToView
 )
 
-func main() {
+func runConstants() {
 	const userRights byte = hasAccessToView | hasAccessToEdit
 	fmt.Printf("User roles: %b\n", userRights)
 	fmt.Printf("Is user root? Result: %v\n", userRights & isRoot == isRoot)
 	fmt.Printf("Has access to view? Result: %v\n", userRights & hasAccessToView == hasAccessToView)
 	fmt.Printf("Has access to delete? Result: %v\n", userRights & hasAccessToDelete == hasAccessToDelete)
+}
+
+func runArray() {
+	// Manipulations with arrays: without & pointing to different cells.
+	array := []float32 { 5, 4, 3, 1 }
+	var arrayCopy []float32 = array
+	fmt.Println("Arrays:")
+	fmt.Println(array)
+	fmt.Println(arrayCopy)
+
+	sliceCopy := array[:]
+	sliceCopy0t2 := array[0:2]
+	sliceCopy2tLen := array[2:len(array)]
+	fmt.Println("Slice & Array:")
+	fmt.Println(array)
+	fmt.Println(sliceCopy)
+	fmt.Println(sliceCopy0t2)
+	fmt.Println(sliceCopy2tLen)
+
+	// Manipulations. Be careful, array and slice pointing to the same data.
+	sliceCopy[0] = 3
+	fmt.Println("Slice & Array. Same cells:")
+	fmt.Println(array)
+	fmt.Println(sliceCopy)
+}
+
+func main() {
+	runConstants()
+	runArray()
 }
