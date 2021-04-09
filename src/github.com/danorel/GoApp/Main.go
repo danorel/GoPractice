4b9996bb69bdd8a23fc120fc0131474d23bde446
danorel <mail.ordan@gmail.com>
2021-04-09 15:01:39 +0300

    [Maps & Structs]: Experimentation done on maps annd structs basics.

--- src/github.com/danorel/GoApp/Main.go
+++ src/github.com/danorel/GoApp/Main.go
@@ -17,7 +17,7 @@ func runConstants() {
 	fmt.Printf("Has access to delete? Result: %v\n", userRights & hasAccessToDelete == hasAccessToDelete)
 }
 
-func runArray() {
+func runArraysAndSlices() {
 	// Manipulations with arrays: without & pointing to different cells.
 	array := []float32 { 5, 4, 3, 1 }
 	var arrayCopy []float32 = array
@@ -25,6 +25,7 @@ func runArray() {
 	fmt.Println(array)
 	fmt.Println(arrayCopy)
 
+	// Manipulations with slices: cutting the slices with indices as in Python
 	sliceCopy := array[:]
 	sliceCopy0t2 := array[0:2]
 	sliceCopy2tLen := array[2:len(array)]
@@ -41,7 +42,68 @@ func runArray() {
 	fmt.Println(sliceCopy)
 }
 
+func runMaps() {
+	// Map declaration. Way #1.
+	someMap := make(map[string]int)
+
+	// Map initialization.
+	someMap = map[string]int {"Key": 12313}
+	fmt.Println(someMap)
+
+	// Map declaration. Way #2.
+	anotherMap := map[string]int {"Key": 123123}
+	fmt.Println(anotherMap)
+
+	// Map value extraction.
+	fmt.Println(anotherMap["Key"])
+
+	// Map addition.
+	anotherMap["AnotherKey"] = 14
+	fmt.Println(anotherMap["AnotherKey"])
+
+	// Check Map key order.
+	fmt.Println(anotherMap)
+
+	// Delete the key.
+	pop, ok := anotherMap["AnotherKey"]
+	fmt.Println(pop, ok)
+	if ok {
+		delete(anotherMap, "AnotherKey")
+	}
+	fmt.Println(anotherMap)
+
+	// Copying another map
+	anotherMapCopy := anotherMap
+	fmt.Println(anotherMapCopy)
+}
+
+type Doctor struct {
+	number int
+	actorName string
+	companions []Doctor
+}
+
+func runStructs() {
+	assistantDoctor := Doctor{
+		number: 1,
+		actorName: "Your Assistant",
+		companions: []Doctor {},
+	}
+
+	fmt.Println(assistantDoctor)
+
+	aDoctor := Doctor{
+		number: 5,
+		actorName: "Danyil Orel",
+		companions: []Doctor{ assistantDoctor },
+	}
+
+	fmt.Println(aDoctor)
+}
+
 func main() {
 	runConstants()
-	runArray()
+	runArraysAndSlices()
+	runMaps()
+	runStructs()
 }
